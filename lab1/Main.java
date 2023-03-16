eaa211a91001474864043fc95a62d25844924e00
VecaBTJ <vedran.moskov@fer.hr>
2023-03-16 15:53:01 +0100

    lab1 main

---
+++ lab1/Main.java
@@ -0,0 +1,9 @@
+// klasa za testiranje funkcija
+import oop.lab1.MorseDecoder;
+import oop.lab1.MaxSubArray;
+public class Main {
+    public static void main(String[] args) {
+
+        return 0;
+    }
+}
