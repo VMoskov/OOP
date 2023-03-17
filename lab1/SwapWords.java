0a4be370292d8dc13c457e21abf062d3acfa7045
VecaBTJ <vedran.moskov@fer.hr>
2023-03-17 19:25:22 +0100

    swapWords

--- lab1/SwapWords.java
+++ lab1/SwapWords.java
@@ -1,5 +1,17 @@
 public class SwapWords {
-    public static String SwapWords(String text, String from, String to) {
+    public static String swapWords(String text, String from, String to) {
         return text.replace(from, to);
+//        String newText = "";
+//        int i = 0;
+//        while (i < text.length()) {
+//            if (text.substring(i, Math.min(i + fromStr.length(), text.length())).equals(fromStr)) {
+//                newText += toStr;
+//                i += fromStr.length();
+//            } else {
+//                newText += text.charAt(i);
+//                i++;
+//            }
+//        }
+//        return newText;
     }
 }
